da8445bd878742568235e4a11666d3022df301e1
reidomath <mat.s.ferreira@gmail.com>
2020-07-04 23:37:57 -0300

    criação da paragina para estoque e ireção com controller

---
+++ src/main/java/com/salaoarrazus/controller/page/EstoqueController.java
@@ -0,0 +1,44 @@
+package com.salaoarrazus.controller.page;
+
+import com.salaoarrazus.domain.model.Atendimento;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/organizacao/estoque")
+public class EstoqueController {
+
+	@GetMapping()
+	public String estoque(ModelMap model) throws Exception {
+		return "organizacao/estoque";
+	}
+
+	@GetMapping("/editar/{id}/estoque")
+	public ModelAndView editarEstoque(@PathVariable("id") Long id) {
+		return null;
+	}
+
+	@GetMapping("/adicionar")
+	public ModelAndView adicionarEstoque(Atendimento atendimento) {
+		return null;
+	}
+
+	@PostMapping("/save")
+	public String save(Atendimento atendimento, @RequestParam("estoqueId") Long id) {
+		return null;
+	}
+
+	@DeleteMapping("/remover/{id}/estoque")
+	public String delete(@PathVariable("id") Long id) {
+		return null;
+	}
+	
+}
