5f2dbf935c4b24bc6ba537c4d91a92b89b883a16
reidomath <mat.s.ferreira@gmail.com>
2020-07-08 20:59:16 -0300

    index para fluxo de caixa

--- src/main/java/com/salaoarrazus/controller/page/FluxoCaixaController.java
+++ src/main/java/com/salaoarrazus/controller/page/FluxoCaixaController.java
@@ -1,5 +1,10 @@
 package com.salaoarrazus.controller.page;
 
+import com.salaoarrazus.service.AtendimentoService;
+import com.salaoarrazus.service.ProdutoService;
+import com.salaoarrazus.service.config.PeriodicidadeAtendimentosEnum;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,9 +14,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/organizacao/fluxo-caixa")
 public class FluxoCaixaController {
 
+	@Autowired
+	ProdutoService produtoService;
+
+	@Autowired
+	AtendimentoService atendimentoService;
 
 	@GetMapping()
 	public String fluxo(ModelMap model) throws Exception {
+		model.addAttribute("atendimentos", atendimentoService.getAtendimentos(PeriodicidadeAtendimentosEnum.TODOS));
+		model.addAttribute("itens", produtoService.getProdutos());
 		return "organizacao/index-fluxo-de-caixa";
 	}
 	
