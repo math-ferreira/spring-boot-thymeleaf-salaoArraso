27f04c1639713e21b115bc2dc6d8ff4e522cf5bc
reidomath <mat.s.ferreira@gmail.com>
2020-07-18 12:34:09 -0300

    adicionando logs com slf4j e logback

--- src/main/java/com/salaoarrazus/SalaoArrazusApplication.java
+++ src/main/java/com/salaoarrazus/SalaoArrazusApplication.java
@@ -1,12 +1,16 @@
 package com.salaoarrazus;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SalaoArrazusApplication {
 
+	private static Logger logger = LoggerFactory.getLogger(SalaoArrazusApplication.class);
 	public static void main(String[] args) {
+		logger.info("### - Iniciando aplicacao");
 		SpringApplication.run(SalaoArrazusApplication.class, args);
 	}
 
